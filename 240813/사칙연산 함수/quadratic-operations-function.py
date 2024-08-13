051c44a0701efd94bac8c47eb787f43088801d87
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-08-13 19:47:36 +0900

    Title: 사칙연산 함수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240813/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,20 @@
+sik = input()
+a, op, b = sik.split()
+a = int(a)
+b = int(b)
+res = 0
+
+sik += " = "
+
+if op == "+":
+    sik = f"{sik}{a + b}"
+elif op == "-":
+    sik = f"{sik}{a - b}"
+elif op == "*":
+    sik = f"{sik}{a * b}"
+elif op == "/":
+    sik = f"{sik}{a // b}"
+else:
+    sik = str(False).lower()
+
+print(sik)
