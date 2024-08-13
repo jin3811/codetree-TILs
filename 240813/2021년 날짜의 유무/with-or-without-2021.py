26c22588b4be2467e7ba7f6e223b27cc453a2185
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-08-13 20:40:35 +0900

    Title: 2021년 날짜의 유무, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/2021년 날짜의 유무/with-or-without-2021.py
+++ 240813/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,4 +1,4 @@
 m, d = map(int, input().split())
-days = [31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-if m < 0 or m > 12 or d < 0 or d > days[m - 1]: print("No")
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if  m > 12 or d > days[m - 1]: print("No")
 else: print("Yes")
