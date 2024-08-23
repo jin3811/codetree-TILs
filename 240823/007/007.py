dba37ea25920994ed438c0b3a27ec0df9d486a0f
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-08-23 15:35:23 +0900

    Title: 007, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240823/007/007.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, code, meeting, time):
+        self.code = code
+        self.meeting = meeting
+        self.time = time
+
+    def __repr__(self):
+        return f"""code : {self.code}
+meeting : {self.meeting}
+time : {self.time}"""
+
+print(User(*input().split()))
