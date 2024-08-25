347c2e1ef0a17dca2ce64d5de94a5c248b7e44df
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-08-25 21:47:03 +0900

    Title: 특정 수와 근접한 합, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240825/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,13 @@
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+total = sum(arr)
+answer = 0
+
+for i in range(0, n):
+    for j in range(i, n):
+        temp = total - arr[i] - arr[j]
+        
+        if abs(s - answer) > abs(s - temp):
+            answer = temp
+
+print(abs(s - answer))
