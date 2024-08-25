6530a2487588fe80b69fa27608d38415d638e04e
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-08-25 19:42:40 +0900

    Title: 진수 to 진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240825/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,10 @@
+def sol(n : int, base : int) -> str :
+    res = ""
+    while n : 
+        res.append(n % base)
+        n //= base
+    
+    return "".join(map(str, res))
+
+a,b = map(int, input().split())
+print(sol(int(input(), a), b))
